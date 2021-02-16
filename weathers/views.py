e91531624f59fa888bc1743c9c43989be79fc11b
hyun <qkrgustnq@naver.com>
2021-02-16 14:50:31 +0900

    edit weather

--- weathers/views.py
+++ weathers/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .utils import weather
 
 def weather_detail(request):
-    location="seoul"
+    location = request.GET.get('location', 'seoul')
     context = weather(location)
     return render(request, 'weather/weather.html', context)
