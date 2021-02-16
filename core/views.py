b4652aa55c34e9d3b8310b89845578835151f588
hyun <qkrgustnq@naver.com>
2021-02-16 14:47:36 +0900

    edit weather

--- core/views.py
+++ core/views.py
@@ -8,14 +8,13 @@ from event.utils import Calendar
 
 def index(request):
     location = request.GET.get('location', 'seoul')
-
     context = weather(location)
+
     context['recommend'] = recommend(context['weatherInfo']['temp'])
     tmp = recommend_music(context['weatherInfo']['temp'], context['weatherInfo']['description'])
     context['recommend_music1'] = tmp[0]
     context['recommend_music2'] = tmp[1]
-    # print(recommend_music(context['weatherInfo']['temp'], context['weatherInfo']['description']))
-
+    
     user = request.user.id
     d = get_date(request.GET.get('month', None))
     cal = Calendar(d.year, d.month)
