5f7115d4ca54bafe3e078a027ce78be50c1567b6
ellendusrud <31755208+ellendusrud@users.noreply.github.com>
2021-02-19 08:12:48 +0900

    login 일단 commit 4

--- users/views.py
+++ users/views.py
@@ -16,7 +16,6 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
 from django.core.mail import EmailMessage
 from django.utils.encoding import force_bytes, force_text
-from .tokens import account_activation_token
 
 
 def index(request):
@@ -34,41 +33,11 @@ def signup(request):
             email = form.cleaned_data.get("email") 
             password = form.cleaned_data.get("password1") 
             user = authenticate(username=email, password=password) 
-            if user is not None: 
-            # user =
-            # current_site = get_current_site(request)
-            # message = render_to_string('accounts/activateEmail.html', {
-            #     'user': user,
-            #     'domain': current_site.domain,
-            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            #     'token': account_activation_token.make_token(user),
-            # })
-            # mail_title = "계정 활성화 확인 이메일"
-            # mail_to = request.POST["email"]
-            # email = EmailMessage(mail_title, message, to=[mail_to])
-            # email.send()
+            if user is not None:
                 login(request, user) 
                 return redirect('/')
 
     return render(request, 'users/signup.html', {'form': form})
-
-# 이메일 인증
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoseNotExsit):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        if 'next' in request.POST:
-            return redirect(request.POST.get('next'))
-        else:
-            return redirect('/')
-    else:
-        return render(request, '/', {'error' : '계정 활성화 오류'})
 
 def signin(request):
     form = LoginForm()
@@ -91,4 +60,3 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('/')
-    
