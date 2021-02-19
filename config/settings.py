55af3a051a553a21553ef5ff1daf6ed2484607b7
hyun <qkrgustnq@naver.com>
2021-02-19 09:10:15 +0900

    edit

--- config/settings.py
+++ config/settings.py
@@ -31,21 +31,25 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
-
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+THIRD_PARTY_APPS = [
+    'bootstrap4',
+]
+PROJECT_APPS = [
     'users',
     'weathers',
     'recommends',
     'event',
     'core',
-    'bootstrap4',
 ]
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
